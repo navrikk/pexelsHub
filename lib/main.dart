2476d261b88d1c514be607c55d7411b9e73d18c9
Nikshep AV <nikshep.av@gmail.com>
2020-11-15 01:30:34 +0530

    Use dotenv for app configuration (#5)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:pexelsHub/views/home_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await DotEnv().load('.env');
   runApp(MyApp());
 }
 
